82345bd5def9b10d900208c934352c7ef9f8c609
Paola Nassisi <paola.nassisi@gmail.com>
2017-07-07 17:51:43 +0200

    Check style bug fixed.

--- data-analytics-ncdump-portlet/src/main/java/it/cmcc/indigo/dataanalytics/NCDumpPortlet.java
+++ data-analytics-ncdump-portlet/src/main/java/it/cmcc/indigo/dataanalytics/NCDumpPortlet.java
@@ -1,24 +1,23 @@
 /**
  * *********************************************************************
- * Copyright (c) 2017: Euro Mediterranean Center on Climate Change (CMCC) Foundation -
- * INDIGO-DataCloud
+ * Copyright (c) 2017: Euro Mediterranean Center on Climate Change (CMCC)
+ * Foundation - INDIGO-DataCloud
  *
  * See http://www.cmcc.it for details on the copyright holders.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  **********************************************************************
- */
+ **/
 
 package it.cmcc.indigo.dataanalytics;
 
@@ -34,25 +33,25 @@ import javax.portlet.RenderResponse;
 import org.osgi.service.component.annotations.Component;
 
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=Data Analytics",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=Data analytics ncdump portlet",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+   immediate = true,
+   property = {
+      "com.liferay.portlet.display-category=Data Analytics",
+      "com.liferay.portlet.instanceable=true",
+      "javax.portlet.display-name=Data analytics ncdump portlet",
+      "javax.portlet.init-param.template-path=/",
+      "javax.portlet.init-param.view-template=/view.jsp",
+      "javax.portlet.resource-bundle=content.Language",
+      "javax.portlet.security-role-ref=power-user,user"
+   },
+   service = Portlet.class
 )
 
 public class NCDumpPortlet extends MVCPortlet {
-	public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
-		throws IOException, PortletException {
-		
-		renderRequest.setAttribute("url", "http://remotetest.unidata.ucar.edu/dts/test.01.das");
-		
-		super.doView(renderRequest, renderResponse);
-	}
+   public final void doView(final RenderRequest renderRequest,
+      final RenderResponse renderResponse)
+      throws IOException, PortletException {
+         renderRequest.setAttribute("url",
+            "http://remotetest.unidata.ucar.edu/dts/test.01.das");
+         super.doView(renderRequest, renderResponse);
+   }
 }
