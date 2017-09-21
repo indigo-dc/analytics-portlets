9f2900f8f1489f7ab4ab395967f0224b4776fa3c
Paola Nassisi <paola.nassisi@gmail.com>
2017-09-21 23:10:32 +0200

    Checkstyle requirements for monitoring portlet almost addressed.

--- data-analytics-monitoring-portlet/src/main/java/it/cmcc/indigo/dataanalytics/portlet/MonitoringPortlet.java
+++ data-analytics-monitoring-portlet/src/main/java/it/cmcc/indigo/dataanalytics/portlet/MonitoringPortlet.java
@@ -1,7 +1,7 @@
 /**
  * *********************************************************************
- * Copyright (c) 2017: Euro Mediterranean Center on Climate Change (CMCC) Foundation -
- * INDIGO-DataCloud
+ * Copyright (c) 2017: Euro Mediterranean Center on Climate Change (CMCC)
+ * Foundation - INDIGO-DataCloud
  *
  * See http://www.cmcc.it for details on the copyright holders.
  *
@@ -16,15 +16,12 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
  **********************************************************************
  */
 
 package it.cmcc.indigo.dataanalytics.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,56 +40,61 @@ import org.osgi.service.component.annotations.Component;
  * Main class for the Future Gateway tasks monitoring.
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=Data Analytics",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.name=monitoring",
-		"javax.portlet.display-name=Experiments monitoring",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+    immediate = true,
+    property = {
+        "com.liferay.portlet.display-category=Data Analytics",
+        "com.liferay.portlet.instanceable=true",
+        "javax.portlet.name=monitoring",
+        "javax.portlet.display-name=Experiments monitoring",
+        "javax.portlet.init-param.template-path=/",
+        "javax.portlet.init-param.view-template=/view.jsp",
+        "javax.portlet.resource-bundle=content.Language",
+        "javax.portlet.security-role-ref=power-user,user"
+    },
+    service = Portlet.class
 )
 
 public class MonitoringPortlet extends MVCPortlet {
-	
-	private String token = null;
-	
-	@Override
-    public void serveResource(ResourceRequest resourceRequest,
-    ResourceResponse resourceResponse) throws IOException, PortletException {
-		
-        token = resourceRequest.getParameter("token");
-        
-//    	URL obj = new URL("http://cloud144.ncg.ingrid.pt/apis/v1.0/tasks");
-    	URL obj = new URL("https://fgw01.ncg.ingrid.pt/apis/v1.0/tasks");
-        HttpURLConnection con = (HttpURLConnection)obj.openConnection();
-        con.setRequestMethod("GET");
-		con.setRequestProperty("Authorization", "Bearer " + token);
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL: " + obj.toString());
-		System.out.println("Response Code : " + responseCode);
-		
-		if(responseCode==201 || responseCode==200) {
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			String jsonResponse = response.toString();
-			int index = jsonResponse.indexOf("[");
-			jsonResponse = jsonResponse.substring(index);
-			jsonResponse = jsonResponse.substring(0, jsonResponse.length()-1);
-			resourceResponse.getWriter().write(jsonResponse);
-		}
-		else
-			System.out.println("Unable to connect to the URL " + obj.toString());
-    } 
+    /**
+     * The reference to the token.
+     */
+    private String token = null;
+
+    @Override
+    public final void serveResource(final ResourceRequest resourceRequest,
+        final ResourceResponse resourceResponse) throws IOException,
+        PortletException {
+
+        token = resourceRequest.getParameter("token");
+
+//      URL obj = new URL("http://cloud144.ncg.ingrid.pt/apis/v1.0/tasks");
+        URL obj = new URL("https://fgw01.ncg.ingrid.pt/apis/v1.0/tasks");
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Authorization", "Bearer " + token);
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL: " + obj.toString());
+        System.out.println("Response Code : " + responseCode);
+
+        if (responseCode == 201 || responseCode == 200) {
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            String jsonResponse = response.toString();
+            int index = jsonResponse.indexOf("[");
+            jsonResponse = jsonResponse.substring(index);
+            jsonResponse = jsonResponse.substring(0, jsonResponse.length() - 1);
+            resourceResponse.getWriter().write(jsonResponse);
+        } else {
+            System.out.println("Unable to connect to the URL "
+                + obj.toString());
+        }
+    }
 }
--- data-analytics-monitoring-portlet/src/main/java/it/cmcc/indigo/dataanalytics/portlet/action/GetOutputPathMVCActionCommand.java
+++ data-analytics-monitoring-portlet/src/main/java/it/cmcc/indigo/dataanalytics/portlet/action/GetOutputPathMVCActionCommand.java
@@ -1,3 +1,24 @@
+/**
+ * *********************************************************************
+ * Copyright (c) 2017: Euro Mediterranean Center on Climate Change (CMCC)
+ * Foundation - INDIGO-DataCloud
+ *
+ * See http://www.cmcc.it for details on the copyright holders.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ **********************************************************************
+ */
+
 package it.cmcc.indigo.dataanalytics.portlet.action;
 
 import java.io.BufferedReader;
@@ -23,77 +44,92 @@ import com.liferay.portal.kernel.util.ParamUtil;
  * Main class for the Future Gateway tasks monitoring.
  */
 @Component(
-		immediate = true,
-        property = {
-                "javax.portlet.name=monitoring",
-                "mvc.command.name=/enes/getOutputPath",
-                "javax.portlet.supported-publishing-event=getpath;http://cloud144.ncg.ingrid.pt"
-        },
-        service = MVCActionCommand.class	
+    immediate = true,
+    property = {
+        "javax.portlet.name=monitoring",
+        "mvc.command.name=/enes/getOutputPath",
+        "javax.portlet.supported-publishing-event=getpath;"
+        + "http://cloud144.ncg.ingrid.pt"
+    },
+    service = MVCActionCommand.class
 )
 
 public class GetOutputPathMVCActionCommand extends BaseMVCActionCommand {
-	
-	private String token = null;
-	private Integer taskid = null;
 
-	@Override
-	protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
-		System.out.println("action!!!");
-		token = ParamUtil.getString(actionRequest, "token");
-		taskid = ParamUtil.getInteger(actionRequest, "taskid");
-		System.out.println("taskid: " + taskid);
-		
-		URL obj = new URL("https://fgw01.ncg.ingrid.pt/apis/v1.0/tasks/" + taskid);
-        HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+    /**
+     * The reference to the token.
+     */
+    private String token = null;
+
+    /**
+     * The reference to the selected ensemble task ID.
+     */
+    private Integer taskid = null;
+
+    @Override
+    protected final void doProcessAction(final ActionRequest actionRequest,
+        final ActionResponse actionResponse) throws Exception {
+        System.out.println("action!!!");
+        token = ParamUtil.getString(actionRequest, "token");
+        taskid = ParamUtil.getInteger(actionRequest, "taskid");
+        System.out.println("taskid: " + taskid);
+
+        URL obj = new URL("https://fgw01.ncg.ingrid.pt/apis/v1.0/tasks/"
+            + taskid);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
-		con.setRequestProperty("Authorization", "Bearer " + token);
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL: " + obj.toString());
-		System.out.println("Response Code : " + responseCode);
-		
-		if(responseCode==201 || responseCode==200) {
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
+        con.setRequestProperty("Authorization", "Bearer " + token);
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL: " + obj.toString());
+        System.out.println("Response Code : " + responseCode);
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			
-			JSONObject myObject;
-			try {
-				myObject = JSONFactoryUtil.createJSONObject(response.toString());
-				String status = myObject.getString("status");
-    			if (status.equals("DONE")) {
-    				JSONArray myArray = myObject.getJSONArray("output_files");
-    				if (myArray.length() != 0) {
-    					JSONObject fileobj = myArray.getJSONObject(0);
-    					String filename = fileobj.getString("name");
-    					if (filename.contains(".png")) {
-    						String completeurl = fileobj.getString("url");
-    						int index = completeurl.indexOf("&");
-    						String url = completeurl.substring(0, index);
-    						
-    						QName qName = new QName("http://cloud144.ncg.ingrid.pt", "getpath");
-        					actionResponse.setEvent(qName, url + "|" + token);
-        					System.out.println("finished.");
-    					}
-    					else
-    						System.out.println("Output png files not found");
-    				}
-    				else
-    					System.out.println("Output files not found");
-    			}
-    			else
-    				System.out.println("Unable to retrieve the outputs until the status is done.");
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-		}
-		else
-			System.out.println("Unable to connect to the URL " + obj.toString());
-	}
+        if (responseCode == 201 || responseCode == 200) {
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            JSONObject myObject;
+            try {
+                myObject = JSONFactoryUtil.createJSONObject(
+                    response.toString());
+                String status = myObject.getString("status");
+                if (status.equals("DONE")) {
+                    JSONArray myArray = myObject.getJSONArray("output_files");
+                    if (myArray.length() != 0) {
+                        JSONObject fileobj = myArray.getJSONObject(0);
+                        String filename = fileobj.getString("name");
+                        if (filename.contains(".png")) {
+                            String completeurl = fileobj.getString("url");
+                            int index = completeurl.indexOf("&");
+                            String url = completeurl.substring(0, index);
+
+                            QName qName = new QName(
+                                "http://cloud144.ncg.ingrid.pt",
+                                "getpath");
+                            actionResponse.setEvent(qName, url + "|" + token);
+                            System.out.println("finished.");
+                        } else {
+                            System.out.println("Output png files not found");
+                        }
+                    } else {
+                        System.out.println("Output files not found");
+                    }
+                } else {
+                    System.out.println("Unable to retrieve the outputs"
+                            + "until the status is done.");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Unable to connect to the URL "
+            + obj.toString());
+        }
+    }
 }
